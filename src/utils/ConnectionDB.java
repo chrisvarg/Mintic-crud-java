7277db1155db0f7cc00f2ce65cc4c62e3af69619
christianvargas <ing.christiand.vargas@gmail.com>
2021-09-07 15:12:53 -0500

    Correction and update Database Cinefilo     - Implement of Json in the class ConnectionDB

--- src/utils/ConnectionDB.java
+++ src/utils/ConnectionDB.java
@@ -1,38 +1,62 @@
 package utils;
 
-import com.mysql.cj.xdevapi.JsonParser;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 /**
  * La clase esta enfocada en realiza la conexión a la base de datos de cinefilo
  * usando la librería Connection
  *
  * @since 2021
- * @author Christian Vargas
+ * @author Christian Vargas (ChrisVarg) Developer
  */
 public  class ConnectionDB {
 
-
     /**
-     * Método que establece la conexión con mysql.
+     * Método estatico que establece la conexión con mysql.
      * Al mismo tiempo recibe las credenciales de acceso en formato Json.
      *
+     * @return connection, resultado de la conexión
      * @autor Christian Vargas (ChrisVarg) Developer
      */
     public static Connection getConnection()
     {
-        JsonParser parser = new JsonParser();
+
+        JSONParser parser = new JSONParser();
         Connection connection = null;
+
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinefilo", "root", "contraseña");
-            if (connection != null){
+            // Procesamiento credenciales (dbCredentials.json)
+            String credentials = System.getProperty("user.dir") + "/src/utils/dbCredentials.json";
+            JSONObject jsonObject = (JSONObject)parser.parse(new FileReader(credentials));
+
+            String host     = (String) jsonObject.get("ip");
+            String port     = (String) jsonObject.get("port");
+            String username = (String) jsonObject.get("user");
+            String password = (String) jsonObject.get("password");
+            String dbURL = "jdbc:mysql://" + host + ":" + port + "/cinefilo";
+
+            // conexión database
+            connection = DriverManager.getConnection(dbURL, username, password);
+            /*if (connection != null){
                 System.out.println("Successfull Connection");
-            }
-        }catch(SQLException e) {
-            System.out.println(e + " nada");
+            }*/
+
+        // Manejo de exceptions para la database y JSON
+        }catch(SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException | ParseException e){
+            e.printStackTrace();
         }
         return connection;
     }
